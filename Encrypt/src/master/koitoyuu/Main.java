b39bfea45c9d742f3c93a7842387e8ed1852e2fc
K0itoyuu <76237220+K0itoyuu@users.noreply.github.com>
2023-01-16 20:27:04 +0800

    加密端ww~

---
+++ Encrypt/src/master/koitoyuu/Main.java
@@ -0,0 +1,46 @@
+package master.koitoyuu;
+
+import master.koitoyuu.utils.EncryptThread;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Kotioyuu233~
+ */
+public class Main {
+    public static void main(String[] args) {
+        File file = new File(args[0]);
+        if (file.exists()) {
+            if (file.isDirectory()) {
+                List<File> files = new ArrayList<>();
+                getAllFile(file,files);
+                for (File file1 : files) {
+                    new EncryptThread(file1).run();
+                }
+                System.out.println("[信息] 嗯哼 解密完成呐~");
+            } else if (file.isFile()) {
+                new EncryptThread(file).run();
+                System.out.println("[信息] 嗯哼 解密完成呐~");
+            } else {
+                System.out.println("[信息] 卧槽 这是歌姬吧!");
+            }
+        } else {
+            System.out.println("[信息] 文件或文件夹不存在呜...");
+        }
+    }
+
+    private static void getAllFile(File file, List<File> allFileList) {
+        File[] fileList = file.listFiles();
+        if (fileList != null) {
+            for (File file1 : fileList) {
+                if (file1.isDirectory()) {
+                    getAllFile(file1, allFileList);
+                } else {
+                    allFileList.add(file1);
+                }
+            }
+        }
+    }
+}
---
+++ Encrypt/src/master/koitoyuu/utils/EncryptThread.java
@@ -0,0 +1,60 @@
+package master.koitoyuu.utils;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.SecureRandom;
+
+/**
+ * @author Koitoyuu233~
+ */
+public class EncryptThread implements Runnable {
+    private final File file;
+    public EncryptThread(File file) {
+        this.file = file;
+    }
+
+    @Override
+    public void run() {
+        try {
+            byte[] bytes = Files.readAllBytes(Paths.get(file.getPath()));
+            bytes = encrypt(bytes);
+            //防止隐藏的文件抛出FileNotFoundException
+            file.delete();
+            file.createNewFile();
+            writeBytes(file,bytes);
+            System.out.println("[加密成功] " + file.getPath());
+        } catch (Exception e) {
+            System.out.println("[加密失败] " + file.getPath());
+        }
+    }
+
+    public void writeBytes(File file,byte[] bytes) throws IOException {
+        Files.write(Paths.get(file.getPath()),bytes);
+    }
+
+    private SecretKey generateKey(byte[] key) throws Exception {
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+        random.setSeed(key);
+        KeyGenerator gen = KeyGenerator.getInstance("AES");
+        gen.init(128, random);
+        return gen.generateKey();
+    }
+
+    private byte[] encrypt(byte[] bytes) {
+        try {
+            String key = "FileEncryptByKoitoyuu233~";
+            SecretKey secKey = generateKey(key.getBytes());
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, secKey);
+            return cipher.doFinal(bytes);
+        } catch (Exception e) {
+            return bytes;
+        }
+    }
+
+}
